413b0ddd4030e638e31d5ed3a204b68d328aae84
ajarry <allan.jarry@mistral.fr>
2025-01-17 14:00:55 +0100

    feat(Singly_Linked_list): add print and lookup procedures

--- src/main.c
+++ src/main.c
@@ -28,6 +28,19 @@ size_t linked_list_len(Linked_List_Node* linked_list_head) {
     return len;
 }
 
+void linked_list_print(Linked_List_Node* linked_list_head) {
+    Linked_List_Node* curr_node = linked_list_head;
+    for (;curr_node != NULL;) {
+        if (curr_node != linked_list_head) {
+            printf(" - %d", curr_node->val);
+        } else {
+            printf(" %d", curr_node->val);
+        }
+
+        curr_node = curr_node->next;
+    }
+}
+
 void linked_list_append(Linked_List_Node* linked_list_head, int val) {
     assert(linked_list_head != NULL && "Linked List head is NULL.");
 
@@ -114,6 +127,24 @@ bool linked_list_remove(Linked_List_Node* linked_list_head, size_t idx, int* rem
     return false;
 }
 
+bool linked_list_lookup(Linked_List_Node* linked_list_head, int needle_val, size_t* found_idx) {
+    assert(linked_list_head != NULL && "Linked List head is NULL.");
+
+    size_t idx = 0;
+    Linked_List_Node* curr_node = linked_list_head;
+    for(;curr_node != NULL;) {
+        if (curr_node->val == needle_val) {
+            *found_idx = idx;
+            return true;
+        }
+
+        curr_node = curr_node->next;
+        idx += 1;
+    }
+
+    return false;
+}
+
 void linked_list_free(Linked_List_Node* linked_list_head) {
     assert(linked_list_head != NULL && "Linked List head is NULL.");
 
@@ -131,51 +162,43 @@ void linked_list_free(Linked_List_Node* linked_list_head) {
 
 // TODO: Valgrind tests, to check memory leaks.
 int main(void) {
+    printf("\n# Linked List:\n");
     Linked_List_Node* linked_list_head = linked_list_new(1);
 
     linked_list_append(linked_list_head, 2);
     linked_list_append(linked_list_head, 3);
     linked_list_append(linked_list_head, 4);
 
+    printf("After appends:");
+    linked_list_print(linked_list_head);
+    printf(" (%llu nodes)\n", linked_list_len(linked_list_head));
+
     linked_list_insert(linked_list_head, 2, 5);
     linked_list_insert(linked_list_head, 5, 6);
     linked_list_insert(linked_list_head, 7, 7);
 
-    printf("\nSingly Linked list before remove:");
-    Linked_List_Node* curr_node = linked_list_head;
-    for (;curr_node != NULL;) {
-        if (curr_node != linked_list_head) {
-            printf(" - %d", curr_node->val);
-        } else {
-            printf(" %d", curr_node->val);
+    printf("After inserts:");
+    linked_list_print(linked_list_head);
+    printf(" (%llu nodes)\n", linked_list_len(linked_list_head));
+
+    size_t found_idx;
+    bool found = linked_list_lookup(linked_list_head, 4, &found_idx);
+    if (found) {
+        printf("Lookup value `4` found at idx: %llu\n", found_idx);
+
+        int removed_val;
+        bool removed = linked_list_remove(linked_list_head, found_idx, &removed_val);
+        if(removed) {
+            printf("removed at %llu = %d\n", found_idx, removed_val);
         }
+        linked_list_remove(linked_list_head, 7, &removed_val);
 
-        curr_node = curr_node->next;
+        printf("After removed:");
+        linked_list_print(linked_list_head);
+        printf(" (%llu nodes)\n", linked_list_len(linked_list_head));
     }
-    printf(" (%llu nodes)", linked_list_len(linked_list_head));
+
     printf("\n");
-
-    int removed_val;
-    bool removed = linked_list_remove(linked_list_head, 1, &removed_val);
-    if(removed) {
-        printf("removed at %d = %d", 1, removed_val);
-    }
-    linked_list_remove(linked_list_head, 7, &removed_val);
-
-    printf("\nSingly Linked list after remove:");
-    curr_node = linked_list_head;
-    for (;curr_node != NULL;) {
-        if (curr_node != linked_list_head) {
-            printf(" - %d", curr_node->val);
-        } else {
-            printf(" %d", curr_node->val);
-        }
-
-        curr_node = curr_node->next;
-    }
-    printf(" (%llu nodes)", linked_list_len(linked_list_head));
-    printf("\n");
-
 
     linked_list_free(linked_list_head);
 
