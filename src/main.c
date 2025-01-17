ee501f0dc053e39a5addd80cc867e016e8bca52d
ajarry <allan.jarry@mistral.fr>
2025-01-17 13:23:58 +0100

    feat(Singly_Linked_List): add insert at pos proc

--- src/main.c
+++ src/main.c
@@ -39,6 +39,38 @@ void linked_list_append(Linked_List_Node* linked_list_head, int val) {
     }
 }
 
+void linked_list_insert(Linked_List_Node* linked_list_head, size_t idx, int val) {
+    assert(linked_list_head != NULL && "Linked List head is NULL.");
+    assert(idx > 0 && "Cannot insert at index 0 with this procedure.");
+
+    size_t i = 1;
+    Linked_List_Node* prev_node = linked_list_head;
+    Linked_List_Node* curr_node = linked_list_head->next;
+    for(;i <= idx;) {
+        if (prev_node == NULL) {
+            return;
+        }
+
+        if (i == idx) {
+            Linked_List_Node* new_node = (Linked_List_Node*) malloc(sizeof(Linked_List_Node));
+            new_node->val              = val;
+            new_node->next             = NULL;
+            prev_node->next            = new_node;
+
+            if(curr_node != NULL) {
+                new_node->next = curr_node;
+            }
+            return;
+        }
+
+        i += 1;
+        prev_node = curr_node;
+        if (curr_node != NULL) {
+            curr_node = curr_node->next;
+        }
+    }
+}
+
 void linked_list_free(Linked_List_Node* linked_list_head) {
     assert(linked_list_head != NULL && "Linked List head is NULL.");
 
@@ -53,12 +85,18 @@ void linked_list_free(Linked_List_Node* linked_list_head) {
     }
 }
 
+
+// TODO: Valgrind tests, to check memory leaks.
 int main(void) {
     Linked_List_Node* linked_list_head = linked_list_new(1);
 
     linked_list_append(linked_list_head, 2);
     linked_list_append(linked_list_head, 3);
     linked_list_append(linked_list_head, 4);
+
+    linked_list_insert(linked_list_head, 2, 5);
+    linked_list_insert(linked_list_head, 5, 6);
+    linked_list_insert(linked_list_head, 7, 7); // FIXME
 
     printf("\nSingly Linked list:");
     Linked_List_Node* curr_node = linked_list_head;
